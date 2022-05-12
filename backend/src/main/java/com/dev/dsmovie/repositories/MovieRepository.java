023e3aa2964a2c0598dc8841d42918ee149bfc84
DimasD <dimasd9753@gmail.com>
2022-05-11 23:07:46 -0300

    Procurar filme

---
+++ backend/src/main/java/com/dev/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.dev.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.dsmovie.Entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+	
+}
