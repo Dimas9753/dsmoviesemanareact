8d6538d2d56d0bbf0cd6a8e3dcb1d40d856201d7
DimasD <dimasd9753@gmail.com>
2022-05-11 21:49:41 -0300

    implantação Model/db

--- backend/src/main/java/com/dev/dsmovie/config/SecurityConfig.java
+++ backend/src/main/java/com/dev/dsmovie/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private Environment env;
-1
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
