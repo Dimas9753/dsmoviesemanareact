8747b8e94f8226f9e8b63912110ef5f9c4efaa17
DimasD <dimasd9753@gmail.com>
2022-05-13 19:56:24 -0300

    Request

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,9 +1,15 @@
+import axios from "axios";
 import MovieCard from "components/moviecards";
 //import MovieScore from "components/moviescore";
 //import MovieStars from "components/moviestars";
 import Pagination from "components/pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+  
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then(response =>{console.log(response.data);});
+  
     return (
         <>
             <Pagination />
