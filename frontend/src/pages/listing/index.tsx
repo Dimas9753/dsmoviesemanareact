bf51a29553ceb5e0ed82d3cfc166bf21857ab881
DimasD <dimasd9753@gmail.com>
2022-05-13 20:35:47 -0300

    Incorporando uses- State/Effect

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -3,12 +3,25 @@ import MovieCard from "components/moviecards";
 //import MovieScore from "components/moviescore";
 //import MovieStars from "components/moviestars";
 import Pagination from "components/pagination";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
   
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(response =>{console.log(response.data);});
+    const [pagenumber,setPageNumber] = useState(0);
+
+    useEffect(() => {
+
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+    .then(response =>{
+       const data = response.data as MoviePage;
+       console.log(data);
+    setPageNumber(data.number);});
+
+    }, []);
+
+    
   
     return (
         <>
